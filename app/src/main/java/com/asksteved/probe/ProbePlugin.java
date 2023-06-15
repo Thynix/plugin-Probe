764979598c9887b5784ac9614ce974ba072e413d
Steve Dougherty <steve@asksteved.com>
2023-06-15 03:31:00 -0400

    Initial commit

---
+++ app/src/main/java/com/asksteved/probe/ProbePlugin.java
@@ -0,0 +1,17 @@
+package com.asksteved.probe;
+
+// TODO: how to import freenet plugin classes?
+/*
+import freenet.l10n.BaseL10n.*;
+import freenet.l10n.*;
+import freenet.pluginmanager.*;
+import freenet.support.*;
+import freenet.support.api.*;
+*/
+
+public class ProbePlugin {
+
+    public static void main(String[] args) {
+    }
+
+}
